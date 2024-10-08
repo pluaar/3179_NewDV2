10ec82de44a057b0e855607856a9af30800ded67
pluaar <127108470+pluaar@users.noreply.github.com>
2024-10-09 01:16:05 +0800

    Create vega_lite_vjs.js

---
+++ vega_lite_vjs.js
@@ -0,0 +1,14 @@
+var choropleth1 = "choroplethmap_details.json";
+vegaEmbed("#map_plot", choropleth1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var continent2 = "continent_bar.json";
+vegaEmbed("continent_plot_2", continent2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var pyramidbar3 = "pyramid_bar_chart.json";
+vegaEmbed("#pyramidbar_plot_3", pyramidbar3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
