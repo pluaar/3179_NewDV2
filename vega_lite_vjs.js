a53ce8234c8184d545c330285217b67c71622d1b
pluaar <127108470+pluaar@users.noreply.github.com>
2024-10-14 15:05:23 +0800

    Update vega_lite_vjs.js

--- vega_lite_vjs.js
+++ vega_lite_vjs.js
@@ -12,3 +12,23 @@ var pyramidbar3 = "pyramid_bar_chart.json";
 vegaEmbed("#pyramidbar_plot_3", pyramidbar3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var heatmap2023 = "Heatmap_2023.json";
+vegaEmbed("#heatmap_plot_2023", heatmap2023).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var heatmap2022= "heatmap_2022.json";
+vegaEmbed("#heatmap_plot_2022", heatmap2022).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var heatmap2021 = "heatmap_2021.json";
+vegaEmbed("#heatmap_plot_2021", heatmap2021).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var heatmap2020 = "heatmap_2020.json";
+vegaEmbed("#heatmap_plot_2020", heatmap2020).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
